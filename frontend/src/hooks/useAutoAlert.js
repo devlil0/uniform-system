637e916b183f22ffdfec12970cad962ac50cc4f8
Murillo Oliveira <upsettxpurple@gmail.com>
2026-04-24 10:58:55 -0300

    move frontend to dedicated branch

--- frontend/src/hooks/useAutoAlert.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export function useAutoAlert(value, setter, ms = 3000) {
-  useEffect(() => {
-    if (!value) return;
-    const id = setTimeout(() => setter(''), ms);
-    return () => clearTimeout(id);
-  }, [value, setter, ms]);
-}
